17f1b29490d800cecd56f106ce47b83ed45a9886
Valeri Savvateev <val@valsmacbook.local>
2012-02-15 23:33:05 -0800

    system.rnd implementation

--- Source/Lunohod/Lunohod/Engine/Objects/XSystem.cs
+++ Source/Lunohod/Lunohod/Engine/Objects/XSystem.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Lunohod.Objects
 {
 	public class XSystem : XObject
 	{
 		private GameEngine game;
+		private static Random random = new Random();
 		
 		public XSystem()
 		{
@@ -50,6 +52,19 @@ namespace Lunohod.Objects
 		public void EndCurrentLevel()
 		{
 			this.CloseCurrentScreen();
+		}
+		
+		public float Rnd(string startStr, string endStr)
+		{
+			if (this.game.Scale.X != this.game.Scale.Y)
+				// Uneven scale, we need to change the engine to ensure that numbers scale properly on both axes.
+				throw new InvalidCastException("Scewed scale. Cannot automatically transform the input number according to scale.");
+				
+				
+			float start = float.Parse(startStr, CultureInfo.InvariantCulture) * this.game.Scale.X;
+			float end = float.Parse(endStr, CultureInfo.InvariantCulture) * this.game.Scale.X;
+			
+			return start + (float)random.NextDouble() * (start - end);
 		}
 	}
 }
