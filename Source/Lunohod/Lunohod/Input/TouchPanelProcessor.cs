8a26b09839cc92a3a0fd13cbe9f905633020dfbb
Valeri Savvateev <val@valsmacbook.local>
2012-02-17 16:11:14 -0800

    fixing the touch issue (finally)

--- Source/Lunohod/Lunohod/Input/TouchPanelProcessor.cs
+++ Source/Lunohod/Lunohod/Input/TouchPanelProcessor.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input.Touch;
+using System.Diagnostics;
 
 namespace Lunohod
 {
 	public class TouchPanelProcessor : InputProcessorBase
 	{
+		private bool released;
+		
 		public TouchPanelProcessor(GameEngine game)
 			: base(game)
 		{
+			this.released = true;
 		}
 		
 		public static Vector2 LastPosition;
@@ -25,13 +29,28 @@ namespace Lunohod
 				var touch = touches[i];
 				
 #if DEBUG
-				Console.WriteLine("Touch: {0},{1} State: {2}", touch.Position.X, touch.Position.Y, touch.State);
+				Debug.WriteLine("Touch: {0},{1} State: {2}", touch.Position.X, touch.Position.Y, touch.State);
 #endif
 				
 				LastPosition = touch.Position;
 				
-				if (touch.State != TouchLocationState.Pressed)
+				if (touch.State == TouchLocationState.Released)
+				{
+					this.released = true;
 					continue;
+				}
+				
+				if (touch.State == TouchLocationState.Invalid)
+					continue;
+				
+				if (touch.State == TouchLocationState.Moved && !this.released)
+					continue;
+				else
+				{
+					Debug.WriteLine("** Issuing a touch! **");
+				}
+				
+				this.released = false;
 				
 				game.ProcessTouch(gameTime, (int)(touch.Position.X / game.Scale.X), (int)(touch.Position.Y / game.Scale.Y));
 			}
