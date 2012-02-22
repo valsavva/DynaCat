f71108ce7621379642a95ba6f87c7c320ef69a9c
Val Savvateev <val@nomnomapps.com>
2012-02-22 02:08:37 -0800

    some documentation update

--- Source/Lunohod/Lunohod/Engine/Objects/XEnemy.cs
+++ Source/Lunohod/Lunohod/Engine/Objects/XEnemy.cs
@@ -7,6 +7,10 @@ using Microsoft.Xna.Framework;
 
 namespace Lunohod.Objects
 {
+    /// <summary>
+    /// Represents a hero's "enemy". Enemy is an entity that can damage hero by the <see cref="Damage"/> amount
+    /// when it attacks him.
+    /// </summary>
     [XmlType("Enemy")]
     public class XEnemy : XElement, IExploding
     {
@@ -18,7 +22,9 @@ namespace Lunohod.Objects
         /// <inheritdoc />
         [XmlAttribute]
         public bool IsExploding { get; set; }
-		
+        /// <summary>
+        /// Makes the enemy attack hero. This will depleat hero's health by the amount specified in the <see cref="Damage"/> attribute.
+        /// </summary>
 		public void Attack()
 		{
 			var hero = GameEngine.Instance.LevelEngine.Hero;
