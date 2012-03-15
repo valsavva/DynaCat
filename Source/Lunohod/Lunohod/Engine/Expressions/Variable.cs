ffe0c670188e3d7c6d3fec8c507e97bf4b96eb95
Valeri Savvateev <val@valsmacbook.local>
2012-03-14 23:00:44 -0700

    fixing some animation logic. changing hero moving animation to use XGE

--- Source/Lunohod/Lunohod/Engine/Expressions/Variable.cs
+++ Source/Lunohod/Lunohod/Engine/Expressions/Variable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Lunohod.Xge;
 
-namespace Lunohod.Windows.Engine.Expressions
+namespace Lunohod.Xge
 {
     public interface IVariable : IExpression
     {
