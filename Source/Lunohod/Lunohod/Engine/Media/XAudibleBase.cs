e165ca9e86d5fa03e958ff2710edfc8ce202c53f
Valeri Savvateev <val@valsmacbook.local>
2012-05-24 09:08:32 -0700

    adding missing file

--- Source/Lunohod/Lunohod/Engine/Media/XAudibleBase.cs
+++ Source/Lunohod/Lunohod/Engine/Media/XAudibleBase.cs
@@ -1,11 +1,45 @@
 using System;
+using System.Xml.Serialization;
+using Lunohod.Objects;
+
+
 namespace Lunohod
 {
-	public class XAudibleBase
+	public abstract class XAudibleBase : XObject, IHasVolume
 	{
-		public XAudibleBase()
+		private double volume;
+
+		[XmlAttribute]
+		public double Volume
 		{
+			get { return this.volume; }
+			set
+			{ 
+				this.volume = value;
+				AdjustVolume();
+			}
 		}
+
+		protected void AdjustVolume()
+		{
+			AdjustVolumeImpl(GameEngine.Instance.IsMute ? 0 : volume);
+		}
+
+		protected abstract void AdjustVolumeImpl(double volume);
+
+		public override void Initialize(InitializeParameters p)
+		{
+			base.Initialize(p);
+
+			GameEngine.Instance.MuteChanged += (sender, e) => AdjustVolume();
+		}
+
+        public override void ReadXml(System.Xml.XmlReader reader)
+        {
+            this.Volume = reader.ReadAttrAsFloat("Volume");
+
+            base.ReadXml(reader);
+        }
 	}
 }
 
